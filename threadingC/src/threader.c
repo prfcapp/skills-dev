5ac609685aef8e65f948d27ef6d6cbb3c3d36c15
prfcapp <156987834+prfcapp@users.noreply.github.com>
2024-07-05 23:14:22 +1000

    threading practice

--- threadingC/src/threader.c
+++ threadingC/src/threader.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <time.h>
 
 pthread_t thread1, thread2, thread3;
 
-volatile int limit = 40;
+
+sem_t sem1;
 
 struct node_stack{
     struct node* head;
@@ -17,12 +21,11 @@ struct node_stack{
 struct node{
     struct node* next;
     int value;
-
 };
 
 void *thread_printer(void* ptr);
-void *thread_producer(void* ptr);
-void *thread_consumer(void* ptr);
+void *thread_producer(void* args);
+void *thread_consumer(void* args);
 
 
 struct node_stack* stack_loader(){
@@ -99,7 +102,6 @@ void remove_node(struct node_stack* stack){
     return;
 }
 
-
 void add_node(struct node_stack* stack, int value){
     if(stack==NULL){
         printf("error - null passed to stack_printer\n");
@@ -119,45 +121,145 @@ void add_node(struct node_stack* stack, int value){
     return;
 }
 
+void *thread_consumer(void* args){
+
+    struct node_stack* stack = (struct node_stack*)args;
+    while(1){
+
+        if(stack == NULL || stack->head == NULL){
+            printf("error removing node...\n");
+            return NULL;
+        }
+
+        sem_wait(&sem1);
+        if(stack->size==1){
+            printf("value %d removed from stack.\n", stack->head->value);
+            free(stack->head);
+            stack->head = NULL;
+            stack->size -= 1;
+            return NULL;
+        }
+
+        //rebase
+        struct node* curs = NULL;
+        curs = stack->head;
+        printf("value %d removed from stack.\n", stack->head->value);
+        stack->head = stack->head->next;
+        free(curs);
+        stack->size -= 1;
+        sem_post(&sem1);
+
+        if(stack->size <= 0){
+            break;
+        }
+        if(stack->size >= 40){
+            break;
+        }
+        stack_printer(stack);
+        sleep(1);
+    }
+
+    return NULL;
+}
+
+
+void *thread_producer(void* args){
+    
+    struct node_stack* stack = (struct node_stack*)args;
+    
+    while(1){
+        if(stack==NULL){
+            printf("error - null passed to stack_printer\n");
+            return NULL;
+        }
+
+        int value = rand() % 20;
+        if(stack->size >= stack->size_limit){
+            printf("error - stack size has hit limit. Disregarding add_node with value %d\n",value);
+            return NULL;
+        }
+
+
+        struct node* new_node = malloc(sizeof(struct node));
+
+
+        new_node->value = value;
+        
+        printf("Adding %d to stack\n",new_node->value);
+        sem_wait(&sem1);
+        
+        new_node->next = stack->head;
+        stack->size += 1;
+        stack->head = new_node;
+
+        sem_post(&sem1);
+
+        if(stack->size>=40){
+            break;
+        }
+        if(stack->size<=0){
+            break;
+        }
+        stack_printer(stack);
+        sleep(1);
+
+    }
+
+    return NULL;
+}
+
+
+
 
 int main(int argc, char** argv){
 
     printf("testing away\n");
 
+    srand(time(NULL));
+
     int thret1, thret2, thret3;
-    
+    sem_init(&sem1,0, 1);
+
     char *message1 = "Thread1";
     char *message2 = "Thread2";
     char *message3 = "Thread3";
 
-    thret1 = pthread_create(&thread1, NULL, thread_printer, (void*)message1);
-    thret2 = pthread_create(&thread2, NULL, thread_printer, (void*)message2);
+    //thret1 = pthread_create(&thread1, NULL, thread_printer, (void*)message1);
+    //thret2 = pthread_create(&thread2, NULL, thread_printer, (void*)message2);
     thret3 = pthread_create(&thread3, NULL, thread_printer, (void*)message3);
 
     // in between
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
+    //pthread_join(thread1, NULL);
+    //pthread_join(thread2, NULL);
     pthread_join(thread3, NULL);
 
     // stack init and print
     struct node_stack* stack = stack_loader(); 
     stack_printer(stack);
 
-    int vals = 21;
+    int vals = 0;
+    /*
     while(1){
         add_node(stack, vals);
         vals++;
-        if(vals>41){
+        if(vals>20){
             break;
         }
     }
+    */
 
-    for(int i = 0;i<45;i++){
-        remove_node(stack);
-    }
+
+    thret1 = pthread_create(&thread1, NULL, thread_consumer, (void*)stack);
+    thret2 = pthread_create(&thread2, NULL, thread_consumer, (void*)stack);
+    thret3 = pthread_create(&thread3, NULL, thread_producer, (void*)stack);
+    
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
 
     stack_printer(stack);
 
+    sem_destroy(&sem1);
 
     return 0;
 }
